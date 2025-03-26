91926bbd4413200ab505c8921c250346ff0b63c2
Ishant Bhatt <ishantbhatt1654@gmail.com>
2025-03-26 17:33:13 +0530

    changes:- animation change test

--- src/app/components/ServiceGrid.jsx
+++ src/app/components/ServiceGrid.jsx
@@ -416,7 +416,7 @@ function ServiceGrid() {
       {/* Header */}
       <motion.div
         className="py-16"
-        initial={{ opacity: 0, y: 50 }}
+        initial={{ opacity: 0, y: 20 }}
         whileInView={{ opacity: 1, y: 0 }}
         viewport={{ once: true, amount: 0.3 }}
         transition={{ duration: 0.6 }}
